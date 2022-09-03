886b5623913760b5d3d397b2e7107f2a8cd188bc
RKamey <90283664+RKamey@users.noreply.github.com>
2022-09-02 23:27:55 -0500

    Add files via upload

--- coladera.js
+++ coladera.js
@@ -1,3 +1,28 @@
 // Coladera de Eratostenes
 // Del 1 al 1000
 
+function coladera() {
+    let numeros = [];
+    let primos = [];
+
+    for (let i = 0; i < 1000; i++) {
+        numeros.push(true);
+    }
+
+    for (let i = 2; i < 1000; i++) {
+        if (numeros[i]) {
+            primos.push(i);
+            for (let j = i * i; j < 1000; j += i) {
+                numeros[j] = false;
+            }
+        }
+    }
+
+    for (let i = 2; i < 1000; i++) {
+        if (numeros[i]) {
+            console.log(i);
+        }
+    }
+}
+
+coladera();
